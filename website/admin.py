c4fe79a2b94f4b4faae01b7e17b13b18c454d7dd
Steve Prater <steve.prater@gmail.com>
2018-03-13 08:14:25 -0500

    Made changes to allow for the side menu. It is now created by specifying a category and and optional parent category. Using this info and a sort 'order' (Added to model) The menu can be generated at runtime from actual site content. Also if 'order' is set to 0, article does not appear in menu.

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Category
 # Register your models here.
 
 
 from django.contrib import admin
 admin.site.register(Article)
+admin.site.register(Category)

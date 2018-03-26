28cd0eb3accf9af00baaa98d1bf0dec324b500f5
Steve Prater <smprater@hlc139.ubuntu>
2018-03-26 15:50:36 -0500

    Added login screen. Added Tests. Added login Template.

--- website/tests.py
+++ website/tests.py
@@ -1,7 +1,7 @@
 # modules
 from django.test import TestCase, Client
 from django.contrib.auth.models import User, Group
-from .models import Article
+from .models import Article, Settings
 
 # Create your tests here.
 
@@ -48,6 +48,8 @@ class MyTestCase(TestCase):
                                       link='http://www.cnn.com',
                                       parent=category,
                                       order=1)
+        settings = Settings.objects.create(site_name='Pratermade', site_tag_line="Tagline here",
+                                          www_root="http://127.0.0.1:8900")
 
 class IndexTest(MyTestCase):
 
@@ -56,3 +58,48 @@ class IndexTest(MyTestCase):
         res = c.get('/')
         self.assertEqual(res.status_code, 200)
 
+
+class MenuCreationTest(MyTestCase):
+
+    def test_for_proper_menu_creation(self):
+        c = Client()
+        res = c.get('/article/article/')
+        menu = [{'title': 'test article', 'slug': 'category', 'sub_menu':
+            [{'title': 'test article', 'slug': 'article'}, {'title': 'test article', 'slug': 'link'}]}]
+        self.assertEqual(menu,res.context['menu'])
+
+
+class BreadCrumbCreationTest(MyTestCase):
+
+    def test_for_proper_breadcrumb_creation(self):
+        c = Client()
+        res = c.get('/article/article/')
+        breadcrumbs = [{'title': 'test article', 'slug': 'category'}, {'title': 'test article', 'slug': 'article'}]
+        self.assertEqual(breadcrumbs,res.context['breadcrumbs'])
+
+
+class PermissionsTests(MyTestCase):
+
+    def test_user_cannot_edit(self):
+        c = Client()
+        session = c.login(username='Frank',password='something')
+        res = c.get('/editArticle/article/')
+        self.assertEqual(res.status_code, 302)
+
+    def test_user_can_edit(self):
+        c = Client()
+        session = c.login(username='Ned', password='somethingelse')
+        res = c.get('/editArticle/article/')
+        self.assertEqual(res.status_code, 200)
+
+    def test_non_editor_in_context(self):
+        c = Client()
+        session = c.login(username='Frank', password='something')
+        res = c.get('/article/article/')
+        self.assertFalse(res.context['can_edit'])
+
+    def test_editor_in_context(self):
+        c = Client()
+        session = c.login(username='Ned', password='somethingelse')
+        res = c.get('/article/article/')
+        self.assertTrue(res.context['can_edit'])

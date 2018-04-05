1d8b13843947aba555c10b1ca819f21632875f71
Steve Prater <steve.prater@gmail.com>
2018-04-05 14:12:10 -0500

    removed file

--- website/forms.py
+++ website/forms.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import Group, User
 class ArticleForm(forms.Form):
     title = forms.CharField(max_length=1024, widget=forms.Textarea(attrs={'class': 'editable'}))
     content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable'}))
-    slug = forms.CharField(max_length=32, widget=forms.Textarea(attrs={}))
+    slug = forms.CharField(max_length=32, widget=forms.TextInput(attrs={}))
     page_type = forms.CharField(widget=forms.Select(attrs={}, choices=PAGETYPES))
     group = forms.ModelChoiceField(queryset=Group.objects.all(), widget=forms.Select(
         attrs={}), required=False)

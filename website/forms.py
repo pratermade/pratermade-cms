26e886f9796120881871df97f368017dcb323f0c
Steve Prater <steve.prater@gmail.com>
2018-04-06 09:02:07 -0500

    Made the file picker work, updated the edit form. Other neat stuff.

--- website/forms.py
+++ website/forms.py
@@ -5,14 +5,14 @@ from django.contrib.auth.models import Group, User
 
 
 class ArticleForm(forms.Form):
-    title = forms.CharField(max_length=1024, widget=forms.Textarea(attrs={'class': 'editable'}))
+    title = forms.CharField(max_length=1024, widget=forms.TextInput())
     content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable'}))
-    slug = forms.CharField(max_length=32, widget=forms.TextInput(attrs={}))
+    # slug = forms.CharField(max_length=32, widget=forms.TextInput(attrs={}))
     page_type = forms.CharField(widget=forms.Select(attrs={}, choices=PAGETYPES))
     group = forms.ModelChoiceField(queryset=Group.objects.all(), widget=forms.Select(
         attrs={}), required=False)
-    owner = forms.ModelChoiceField(queryset=User.objects.all(), widget=forms.Select(
-        attrs={}))
+    # owner = forms.ModelChoiceField(queryset=User.objects.all(), widget=forms.Select(
+    #     attrs={}))
     parent = forms.ModelChoiceField(queryset=Article.objects.all(), widget=forms.Select(
         attrs={}), required=False)
     order = forms.CharField(widget=forms.TextInput(attrs={}), required=False)

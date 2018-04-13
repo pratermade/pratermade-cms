725f9996bdc0ee8435344ac616298b02cf919d5a
Steve Prater <steve.prater@gmail.com>
2018-04-13 10:37:53 -0500

    Added the ability to specify a homepage for the site from the articles. Also add the ability to add an Index type to allow the use of a straight up template for the home page.

--- website/models.py
+++ website/models.py
@@ -8,7 +8,8 @@ from django.contrib.auth.models import Group, User
 PAGETYPES = (
     ('article', 'Article'),
     ('link', 'Link'),
-    ('table_of_contents', 'Table Of Contents')
+    ('table_of_contents', 'Table Of Contents'),
+    ('index','Index')
 )
 
 class Article(models.Model):
@@ -19,7 +20,7 @@ class Article(models.Model):
     slug = models.CharField(max_length=32, unique=True,
                             help_text="Must be one lowercase word. You must "
                                 "populate either slug or link for proper functionality.")
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True, help_text="Anyone is this group "
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True, help_text="Anyone in this group "
                                                                                                 "can edit.")
     owner = models.ForeignKey(User, null=True, on_delete=models.CASCADE, blank=True, help_text="The owner can edit.")
     link = models.URLField(null=True, blank=True, help_text="Use to link to external sites. If used, the field 'slug' "

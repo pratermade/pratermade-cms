e17f84445cb4470c2d66ca65b092dc96ba2c8f6c
Steve Prater <steve.prater@gmail.com>
2018-04-30 14:22:09 -0500

    fixed settings error where there are no settings.

--- ss_cms/loader.py
+++ ss_cms/loader.py
@@ -6,10 +6,11 @@ from website.models import Settings
 class Loader(BaseLoader):
 
     def get_dirs(self):
-        theme = Settings.objects.all()[0].theme
-        self.dirs = []
-        for d in self.engine.dirs:
-            self.dirs.append("{}/{}".format(d, theme))
-            self.dirs.append(d)
+        if Settings.objects.filter().exists():
+            theme = Settings.objects.all()[0].theme
+            self.dirs = []
+            for d in self.engine.dirs:
+                self.dirs.append("{}/{}".format(d, theme))
+                self.dirs.append(d)
 
         return self.dirs if self.dirs is not None else self.engine.dirs
